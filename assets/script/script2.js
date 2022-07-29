abc68900ebd81a9dc4e31e163fa09bf6adde6732
jordanrings <jordanringsakabe@gmail.com>
2022-07-28 21:12:45 -0600

    made the best damn delete button you done ever saw.

--- assets/script/script2.js
+++ assets/script/script2.js
@@ -4,7 +4,6 @@ var playernameinput2El = document.getElementById("player-name-input2");
 var searchBtn2El = document.getElementById("search-btn2");
 var pName = document.getElementById("player-nameEl");
 var pImg = document.getElementById("player-imgEl");
-var pID = document.getElementById("idEl");
 var pTeam = document.getElementById("teamEl");
 var pNumber = document.getElementById("numEl");
 var pHeight = document.getElementById("hgtEl");
@@ -27,20 +26,14 @@ searchForm2.addEventListener("submit", function (event) {
     if (searchText.length > 0) {
         historyItems.push(searchText);
         onFetchPlayerStats();
-        updatePlayerID();
         storeHistory();
         renderSearchHistory();
-        
+
     } else {
         alert("Please enter a valid player name.")
         return;
     }
 });
-
-function updatePlayerID() {
-    var idText = Number(playernameinput2El.value);
-    pID.textContent = idText;
-};
 
 function onFetchPlayerStats() {
     playerID = playernameinput2El.value;
@@ -48,16 +41,8 @@ function onFetchPlayerStats() {
 };
 
 function fetchPlayerStats(playerId) {
-
-    // var getPlayerStatsApi = `https://api.sportsdata.io/v3/nfl/stats/json/DailyFantasyPlayers/${date}?key=${apiKey}`;
     var getPlayerStatsApi2 = `https://api.sportsdata.io/v3/nfl/scores/json/Player/${playerId}?key=1e7382eee81a4fd19631d38f764c1449`
     fetch(getPlayerStatsApi2, {
-        // "method": "GET",
-        // "headers": {
-        //     "Ocp-Apim-Subscription-Key": "1e7382eee81a4fd19631d38f764c1449",
-        //     // "Content-Type": "application/json"
-        // },
-        // "mode": "no-cors"
     }).then(function (response) {
         console.log(response);
         return response.json()
@@ -75,9 +60,13 @@ function fetchPlayerStats(playerId) {
         pInjuries.textContent = data.InjuryStatus
 
     })
-}
+};
 
 function init() {
+    var storedHistory = JSON.parse(localStorage.getItem("historyItems"));
+    if (storedHistory !== null) {
+        historyItems = storedHistory;
+    }
     var searchQuery = window.location.search;
     var urlParams = new URLSearchParams(searchQuery);
     var playerIdInput = urlParams.get("playerid");
@@ -87,13 +76,10 @@ function init() {
     fetchNews();
     renderSearchHistory();
 
-}
+};
 
 init();
 
-// CORY: Declare variables that reference the parts of the HTML code that you will modify with javascript here ⇩
-
-// Examples: 
 title1 = document.getElementById("art-title-1");
 title2 = document.getElementById("art-title-2");
 title3 = document.getElementById("art-title-3");
@@ -106,7 +92,6 @@ artBody3 = document.getElementById("article-body-3");
 readMore1 = document.getElementById("read-more-1");
 readMore2 = document.getElementById("read-more-2");
 readMore3 = document.getElementById("read-more-3");
-// End message
 
 function fetchNews() {
     var getNews = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/news";
@@ -118,24 +103,68 @@ function fetchNews() {
         return response.json()
     }).then(function (data) {
         console.log(data);
-// CORY:Change the text in the news article area 1 here⇩
-
-// Examples: 
-        title1.textContent=data.articles[0].headline;
-        title2.textContent=data.articles[1].headline;
-        title3.textContent=data.articles[2].headline;
-        author1.textContent=data.articles[0].byline;
-        author2.textContent=data.articles[1].byline; 
-        author3.textContent=data.articles[2].byline;
-        artBody1.textContent=data.articles[0].description;
-        artBody2.textContent=data.articles[1].description; 
-        artBody3.textContent=data.articles[2].description;
+        title1.textContent = data.articles[0].headline;
+        title2.textContent = data.articles[1].headline;
+        title3.textContent = data.articles[2].headline;
+        author1.textContent = data.articles[0].byline;
+        author2.textContent = data.articles[1].byline;
+        author3.textContent = data.articles[2].byline;
+        artBody1.textContent = data.articles[0].description;
+        artBody2.textContent = data.articles[1].description;
+        artBody3.textContent = data.articles[2].description;
         readMore1.setAttribute("href", data.articles[0].links.web.href);
         readMore2.setAttribute("href", data.articles[1].links.web.href);
         readMore3.setAttribute("href", data.articles[2].links.web.href);
     })
 };
 
-function renderSearchHistory(){
-    
-}
+function renderSearchHistory() {
+    var searchHistory = document.getElementById("search-history");
+    searchHistory.innerHTML = "";
+    for (let i = 0; i < historyItems.length; i++) {
+        var historyItem = historyItems[i];
+        var li = document.createElement('li');
+        var button = document.createElement('button');
+        var deleteButton =document.createElement('button');
+        button.setAttribute("class", "searchHistoryButton button");
+        button.textContent = historyItem;
+        deleteButton.textContent="X";
+        deleteButton.setAttribute("class", "button is-dark is-small");
+        deleteButton.setAttribute("style", "font-size: 16px; transform:translate(50px); border-radius: 8px;");
+        li.setAttribute("data-index", i);
+        li.append(button);
+        button.append(deleteButton);
+        // When appending list items, make the inner text the name and id and not just the number.
+        searchHistory.append(li);
+
+    }
+
+    var searchHistoryBtn = document.getElementsByClassName("searchHistoryButton");
+    console.log(searchHistoryBtn.length);
+    for (let i = 0; i < searchHistoryBtn.length; i++) {
+        searchHistoryBtn[i].addEventListener("click", function (event) {
+            playernameinput2El.value = event.target.innerHTML;
+            searchForm2.submit(event);
+            
+
+        console.log("hello");
+        })
+       
+    }
+
+};
+
+searchHistoryBtn.addEventListener("click", function(event){
+    var element = event.target;
+    if(element.matches("button") === true) {
+        var index = element.parentElement.getAttribute("data-index");
+        historyItems.splice(index, 1);
+     
+        storeHistory();   
+        renderSearchHistory();
+    }
+});
+
+function storeHistory() {
+    localStorage.setItem("historyItems", JSON.stringify(historyItems));
+};
